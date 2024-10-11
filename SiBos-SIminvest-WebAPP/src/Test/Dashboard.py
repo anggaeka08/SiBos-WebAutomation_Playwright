f54a2e2ed586dcd7c00588763ca995fb35ef8e13
anggaeka08 <43332690+anggaeka08@users.noreply.github.com>
2024-10-11 11:09:31 +0700

    Add files via upload
    add test file login-logout and menu dashboard.

---
+++ SiBos-SIminvest-WebAPP/src/Test/Dashboard.py
@@ -0,0 +1,44 @@
+from playwright.sync_api import sync_playwright
+
+from playwright.sync_api import sync_playwright
+
+
+def test_login_with_valid():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False, slow_mo=1500)
+        page = browser.new_page()
+        page.goto("https://stg-sibos.siminvest.co.id")
+
+        # Locate the username and password fields by their 'id' or 'name' attributes and fill them
+        page.fill("#username", "admin@example.com")  # Fills in the username field using its id
+        page.fill("#password", "admin123")  # Fills in the password field using its id
+        page.click("button:has-text('Sign In')")
+        page.click("p:has-text('Referral Reward')")
+        page.click("span:has-text('Sign Out')")
+        page.wait_for_timeout(5000)  # Wait for 5 seconds to observe the page
+        #input("Press Enter to close the browser...")
+
+        browser.close()
+if __name__ == "__main__":
+    test_login_with_valid()
+
+
+def test_login_with_invalid_username():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False, slow_mo=1500)
+        page = browser.new_page()
+        page.goto("https://stg-sibos.siminvest.co.id")
+
+        # Locate the username and password fields by their 'id' or 'name' attributes and fill them
+        page.fill("#username", "admin@example")  # Fills in the username field using its id
+        page.fill("#password", "admin123")  # Fills in the password field using its id
+        page.click("button:has-text('Sign In')")
+        page.wait_for_timeout(5000)  # Wait for 5 seconds to observe the page
+        input("Press Enter to close the browser...")
+
+        browser.close()
+if __name__ == "__main__":
+    test_login_with_invalid_username()
+
+
+
